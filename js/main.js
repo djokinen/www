ee6c1c0fa0a4d464f091ffcd355bc631313d85b9
djokinen <djokinen@yahoo.com>
2016-11-29 23:41:06 -0500

    round two

---
+++ js/main.js
@@ -0,0 +1,47 @@
+﻿$(function () {
+	$("#button").click(function () {
+		_calculateAverage();
+	});
+});
+
+var _calculateAverage = function () {
+	var input = $("#inputValues").val();
+	var grades = input.split(/[\s,-]+/);
+
+	// loop
+	var topGrades = [];
+	var matureGrades = [];
+
+	for (i = 0; i < grades.length; i++) {
+		var grade = parseFloat( grades[i].replace("x", "").replace("X", "").trim());
+		topGrades.push(grade);
+		if (grades[i].includes("x") || grades[i].includes("X")) {
+			matureGrades.push(grade);
+		}
+	}
+
+	// sort in descending order
+	topGrades.sort(function (a, b) { return b - a });
+	matureGrades.sort(function (a, b) { return b - a });
+
+
+	// var avg = sum / elmt.length;
+	// get top 6 grades
+	topGrades = topGrades.slice(0, 6);
+
+	var total = 0;
+	for (var i = 0; i < topGrades.length; i++) {
+		total += topGrades[i];
+	}
+	var avg = total / topGrades.length;
+
+	$("#outputTopSix").text(avg);
+
+	total = 0;
+	for (var i = 0; i < matureGrades.length; i++) {
+		total += matureGrades[i];
+	}
+	var avg = total / matureGrades.length;
+
+	$("#outputMature").text(avg);
+};
