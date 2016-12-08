ad5f9b7d97f2036b848d3eae0a59a79305e264fb
djokinen <djokinen@yahoo.com>
2016-12-08 16:39:08 -0500

    use object array instead of string array

--- js/main.js
+++ js/main.js
@@ -6,16 +6,51 @@
 });
 
 var _calculateAverage = function () {
+	var gradePrefixes = ['x', 'e', 'm', 'b', 'p', 'c'];
 	var input = $("#inputValues").val().trim();
 	var grades = input.split(/[\s,-]+/);
 
 	// loop
 	var topGrades = [];
 	var matureGrades = [];
-
+	var grade; //var grade = {value:85.2, type:"m", required:true};
+	var gradeList = [];
 	for (i = 0; i < grades.length; i++) {
-		var grade = parseFloat(grades[i].replace("x", "").replace("X", "").trim());
-		topGrades.push(grade);
+		grade = null;
+		for (var j = 0; j < gradePrefixes.length; j++) {
+			if (grades[i].indexOf(gradePrefixes[j].toLowerCase()) >= 0 || grades[i].indexOf(gradePrefixes[j].toUpperCase()) >= 0) {
+				grade = {
+					value: parseFloat(grades[i].replace(gradePrefixes[j].toLowerCase(), "").replace(gradePrefixes[j].toUpperCase(), "").trim()),
+					type: gradePrefixes[j].toLowerCase(),
+					required: true
+				};
+				matureGrades.push(grade);
+			}
+		}
+
+		if (grade == null) {
+			grade = {
+				value: parseFloat(grades[i].trim()),
+				type: "a",
+				required: false
+			};
+		}
+
+		topGrades.push(grade.value);
+		gradeList.push(grade);
+
+		//// get the grade value of this item. remove the ammended letter if present
+		//var grade = parseFloat(
+		//	grades[i]
+		//	.replace(gradePrefixes[0].toLowerCase(), "").replace(gradePrefixes[0].toUpperCase(), "")
+		//	.replace(gradePrefixes[1].toLowerCase(), "").replace(gradePrefixes[1].toUpperCase(), "")
+		//	.replace(gradePrefixes[2].toLowerCase(), "").replace(gradePrefixes[2].toUpperCase(), "")
+		//	.replace(gradePrefixes[3].toLowerCase(), "").replace(gradePrefixes[3].toUpperCase(), "")
+		//	.replace(gradePrefixes[4].toLowerCase(), "").replace(gradePrefixes[4].toUpperCase(), "")
+		//	.replace(gradePrefixes[5].toLowerCase(), "").replace(gradePrefixes[5].toUpperCase(), "")
+		//	.trim());
+
+		//topGrades.push(grade);
 
 		/* * * *
 		 * 
@@ -23,15 +58,19 @@ var _calculateAverage = function () {
 		 * if (grades[i].includes("x") || grades[i].includes("X")) { 
 		 * 
 		 * * * */
-		if (grades[i].indexOf("x") >= 0 || grades[i].indexOf("x") >= 0) {
-			matureGrades.push(grade);
-		}
+		//for (var j = 0; j < gradePrefixes.length; j++) {
+		//	if (grades[i].indexOf(gradePrefixes[j].toLowerCase()) >= 0 || grades[i].indexOf(gradePrefixes[j].toUpperCase()) >= 0) {
+		//		matureGrades.push(grade);
+		//	}
+		//}
+
+		// if (grades[i].indexOf("x") >= 0 || grades[i].indexOf("x") >= 0) { matureGrades.push(grade); }
 	}
 
 	// sort in descending order
-	topGrades.sort(function (a, b) { return b - a });
-	matureGrades.sort(function (a, b) { return b - a });
+	topGrades.sort(function (a, b) { return b.grade - a.grade });
 
+	matureGrades.sort(function (a, b) { return b.grade - a.grade });
 
 	// var avg = sum / elmt.length;
 	// get top 6 grades
@@ -39,7 +78,7 @@ var _calculateAverage = function () {
 
 	var total = 0;
 	for (var i = 0; i < topGrades.length; i++) {
-		total += topGrades[i];
+		total += topGrades[i].grade;
 	}
 	var avg = total / topGrades.length;
 
@@ -48,7 +87,7 @@ var _calculateAverage = function () {
 
 	total = 0;
 	for (var i = 0; i < matureGrades.length; i++) {
-		total += matureGrades[i];
+		total += matureGrades[i].grade;
 	}
 	var avg = total / matureGrades.length;
 
